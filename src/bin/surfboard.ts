994401f127c9e2e60d6b4c49fda5e0d307e50c63
Thibault “Adædra” Hamel <contact@adaedra.eu>
2018-12-25 01:51:24 +0100

    Style
    * Small style change

--- src/bin/surfboard.ts
+++ src/bin/surfboard.ts
@@ -3,8 +3,8 @@ import { resolve } from 'path'
 import { cwd } from 'process'
 import { createServer, startDispatcher } from '..'
 
-let configPath = resolve(cwd(), 'surfboard.server')
-let config = require(configPath)
+const configPath = resolve(cwd(), 'surfboard.server')
+const config = require(configPath)
 
 createServer().listen(1337, () => console.log('Server started.'))
 startDispatcher(config.sources)
